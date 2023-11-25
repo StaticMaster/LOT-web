013b50f5a5588706cfbf22b9c928387b1c90ad61
Gikai Andrews <gnaboys@gmail.com>
2023-11-25 18:50:30 -0500

    added starter component(Homepage, Navbar)

--- lot-web/src/App.js
+++ lot-web/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./Homepage/Homepage";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-    
+      <BrowserRouter>
+        <Routes>
+          <Navbar/>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
