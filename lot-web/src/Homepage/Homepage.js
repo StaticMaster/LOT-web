51234f26b2874b5063150e8523b92477d7ced0ce
Gikai Andrews <gnaboys@gmail.com>
2023-11-27 21:41:23 -0500

    adjusted some stuff on homepage

--- lot-web/src/Homepage/Homepage.js
+++ lot-web/src/Homepage/Homepage.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Homepage.css";
+import Typewriter from "typewriter-effect";
 import timain from "../images/ti-background.jpg";
 
 function Homepage() {
   return (
-    <div className="homepage-section">
-      <img src={timain} className="img-fluid" alt="ti-pfp"></img>
-      <p className="text-end">I am Legend of Ti</p>
+    <div>
+      <div className="homepage-section">
+        <img src={timain} className="img-fluid" alt="ti-pfp"></img>
+        <p className="text-end">I am Legend of Ti</p>
+        <Typewriter
+          options={{
+            strings: ["Creator", "Gamer", "Bodybuilder"],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+            delay: 500,
+          }}
+        />
+         <div className="sponsor-section">
+        <p className="text-center">Sponsors</p>
+      </div>
+      </div>
     </div>
   );
 }
